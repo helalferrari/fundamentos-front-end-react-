ca2075caa6663a54b7668e155c7a1cc5ddb7c317
Helal Ferrari Cabral <ferrari.work@gmail.com>
2026-02-16 12:21:06 -0300

    criando meus hello world

---
+++ src/app/nivel-0/page.tsx
@@ -0,0 +1,12 @@
+// export default function Page() {
+//   return <div>Page</div>;
+// }
+import { MeuNome } from "@/components/MeuNome";
+
+const Page = () => (
+    <div>
+        <MeuNome/>
+    </div>
+);
+
+export default Page;
---
+++ src/components/MeuNome.tsx
@@ -0,0 +1,3 @@
+export const MeuNome = () => {
+    return <p>Ferrari</p>;
+};
